e9e5c43a5a04389e1889122641bdb3e6eca9c777
Carter Klein <carterklein13@gmail.com>
2020-09-11 17:36:14 -0400

    Add basic WebSocket functionality

--- goImplement/goImplement.go
+++ goImplement/goImplement.go
@@ -4,10 +4,18 @@ import (
 	"bytes"
 	"encoding/hex"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"math/big"
 	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 /*
@@ -120,7 +128,7 @@ func Produce(data string, attribute string, supertypeID string, skVendor string,
 }
 
 /*
-Consume receives data from the Supertype data marketplace and decrypt it
+Consume receives data from the Supertype data network, re-encrypts, and decrypts it
 This data is source-agnostic, and encrypted end-to-end
 @param attribute to consume data from
 @param supertypeID the vendor's Supertype ID
@@ -230,4 +238,84 @@ func Consume(attribute string, supertypeID string, skVendor string, pkVendor str
 	}
 
 	return &result, nil
+}
+
+/*
+ConsumeWS subscribes this node to the specified attribute(s)
+@param attribute to consume data from
+@param supertypeID the vendor's Supertype ID
+@param skVendor the vendor's secret key
+@param pkVendor the vendor's public key
+*/
+func ConsumeWS(attribute string, supertypeID string, skVendor string, pkVendor string) (*string, error) {
+	// TODO first make a POST reqeust to the server to add attributes to Redis, then subscribe via WebSocket...
+	// Generate hash of secret key to be used as a signing measure for producing/consuming data
+	// skHash := GetSecretKeyHash(skVendor)
+
+	// requestBody, err := json.Marshal(map[string]string{
+	// 	"attribute":   attribute,
+	// 	"supertypeID": supertypeID,
+	// 	"pk":          pkVendor,
+	// 	"skHash":      skHash,
+	// })
+	// if err != nil {
+	// 	return nil, ErrMarshaling
+	// }
+
+	// Establish WebSocket connection between device <-> server
+	interrupt := make(chan os.Signal, 1)
+	var addr = flag.String("addr", "localhost:8080", "http service address")
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/consumeWS"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	i := 1
+	for {
+		select {
+		case <-done:
+			return nil, nil
+		case _ = <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(strconv.Itoa(i)))
+			if err != nil {
+				return nil, nil
+			}
+			i++
+		case <-interrupt:
+			log.Println("interrupt")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				return nil, nil
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return nil, nil
+		}
+	}
 }
