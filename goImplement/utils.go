f4425e6c022315c950da8284b79e05a1c2fd627e
Carter Klein <carterklein13@gmail.com>
2020-10-21 23:02:14 -0400

    Update message types

--- goImplement/utils.go
+++ goImplement/utils.go
@@ -12,6 +12,7 @@ import (
 )
 
 // Encrypt creates the AES key, encrypts it it with GCM, and returns ciphertext and capsule
+// https://play.golang.org/p/4FQBAeHgRs
 func Encrypt(message string, userKey string) (*string, *string, error) {
 	userKeyBytes := []byte(userKey)
 	messageBytes := []byte(message)
@@ -35,30 +36,32 @@ func Encrypt(message string, userKey string) (*string, *string, error) {
 }
 
 // Decrypt recreates the AES key, then decrypts the encrypted data
-func Decrypt(ciphertext string, userKey string) (*string, error) {
+// https://play.golang.org/p/4FQBAeHgRs
+func Decrypt(ciphertext string, userKey string) (*string, *string, error) {
 	// Split ciphertext from iv
 	metadata := strings.Split(ciphertext, "|")
 	ciphertext = metadata[0]
+	attribute := metadata[2]
 	iv, err := base64.StdEncoding.DecodeString(metadata[1])
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	block, err := aes.NewCipher([]byte(userKey[0:32]))
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	data, err := base64.StdEncoding.DecodeString(ciphertext)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	cfb := cipher.NewCFBEncrypter(block, []byte(iv))
 	plaintext := make([]byte, len(data))
 	cfb.XORKeyStream(plaintext, data)
 	res := string(plaintext)
-	return &res, nil
+	return &res, &attribute, nil
 }
 
 // GetSecretKeyHash returns the hashed value of the secret key
