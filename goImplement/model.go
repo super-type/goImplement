3d18d44ed534f5712302d7861b4486e932eb9e19
Carter Klein <carterklein13@gmail.com>
2021-01-08 17:13:00 -0500

    Adding more meaningful API responses from client library

--- goImplement/model.go
+++ goImplement/model.go
@@ -5,14 +5,20 @@ type ObservationRequest struct {
 	Attribute   string `json:"attribute"`
 	Ciphertext  string `json:"ciphertext"`
 	SupertypeID string `json:"supertypeID"`
-	PublicKey   string `json:"pk"`
-	SkHash      string `json:"skHash"`
 	IV          string `json:"iv"`
 }
 
 // ObservationResponse is returned from the server
 type ObservationResponse struct {
 	Ciphertext  string `json:"ciphertext"`
+	DateAdded   string `json:"dateAdded"`
+	PublicKey   string `json:"pk"`
+	SupertypeID string `json:"supertypeID"`
+}
+
+// ConsumeResponse is the value returned to user
+type ConsumeResponse struct {
+	Plaintext   string `json:"plaintext"`
 	DateAdded   string `json:"dateAdded"`
 	PublicKey   string `json:"pk"`
 	SupertypeID string `json:"supertypeID"`
